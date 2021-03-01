a1d40f7f98924096df90f22e95ea9ea3bcf68a78
umamah1mahmoud <umamah.m.94@gmail.com>
2021-03-01 17:00:47 +0200

    keeping my work updated

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
 'use strict'
+
+let yes= 'Yes'.toLowerCase();
+let y= 'Y'.toLowerCase();
+let no= 'No'.toLowerCase();
+let n= 'N'.toLowerCase();
+let myAge=prompt('Am I 30 years old?');
+if (myAge==yes||myAge==y){
+    alert('Sorry, wrong answer.');
+}
+if (myAge==no|| myAge==n){
+   alert('You guessed right!');
+}
+
+console.log(myAge)
+
